5381ffb205fba97424a22995dfab66b326b26b1b
oldmc <oldmcdonnell@gmail.com>
2023-12-16 21:23:33 -0500

    added link specified language limit news add subject control catagory

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
+
 api_key = "ffccde25362e4cfc83655b738f77d832"
-url = "https://newsapi.org/v2/everything?q=tesla&" \
-      "from=2023-11-15&sortBy=publishedAt&apiKey=" \
-      "ffccde25362e4cfc83655b738f77d832"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "from=2023-11-17&" \
+      "sortBy=publishedAt&" \
+      "apiKey=ffccde25362e4cfc83655b738f77d832&" \
+      "language=en"
 #makle requst
 request = requests.get(url)
 
@@ -13,9 +18,11 @@ content = request.json()
 
 #access article titles and descriptions
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
       if article["title"] is not None:
-            body = body + article["title"] + "\n" + str(article["description"]) + 2*"\n"
+            body = "Subject: FUCK TESLA" \
+                   + "\n" + "Today's News" + "\n" + body + article["title"] + "\n" + \
+                   str(article["description"]) + str(article["url"]) + "\n" + 2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
